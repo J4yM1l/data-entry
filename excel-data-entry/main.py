40c98d68bb97a6f5a581790ca501309fcb24b791
j4ym1l <jchrismilton@gmail.com>
2024-08-11 22:45:40 -0400

    calculate balance due

--- excel-data-entry/main.py
+++ excel-data-entry/main.py
@@ -1,5 +1,6 @@
 import tkinter
 from tkinter import ttk
+from tkinter import messagebox
 import os
 import openpyxl
 
@@ -7,43 +8,51 @@ import openpyxl
 formField_list = []
 cwd = os.getcwd()
 def submit_data():
-    accepted = formField_list[8].get()
+    accepted = formField_list[9].get()
     
     if accepted=="Accepted":
         # User info
         firstname = formField_list[0].get()
         lastname = formField_list[1].get()
-        
+        balance = 0
+        total_cost = 5000
         if firstname and lastname:
-            # Todo student id shld be required
-            # TODO: Optimize formField DS
-            student_id = formField_list[2].get()
-            student_address = formField_list[3].get()
-            phone_number = formField_list[4].get()
-            
-            # Course info
-            term_period = formField_list[5].get()
-            class_type = formField_list[6].get()
-            class_number = formField_list[7].get()
-            print("-----------------Student Data---------------")
-            print("First name: ", firstname, "Last name: ", lastname)
-            print("ID: ", student_id, "\nAddress: ", student_address, "\nPhone Number: ", phone_number)
-            print("Term Period: ", term_period, "\nClass Type: ", class_type, "\nLevel: ", class_number)
-            # print("Registration status", registration_status)
-            print("------------------------------------------")
-            # make sure to update the path syntax for other OS
-            filepath = cwd+"\\data.xlsx"
-            
-            if not os.path.exists(filepath):
-                workbook = openpyxl.Workbook()
+            amount_paid = formField_list[5].get()
+            if int(amount_paid) < total_cost:
+                balance = total_cost - int(amount_paid)
+                # Todo student id shld be required
+                # TODO: Optimize formField DS
+                student_id = formField_list[2].get()
+                student_address = formField_list[3].get()
+                phone_number = formField_list[4].get()
+                # amount_paid = formField_list[5].get()
+                # Course info
+                term_period = formField_list[6].get()
+                class_type = formField_list[7].get()
+                class_number = formField_list[8].get()
+                print("---------------Student Data---------------")
+                print("First name:", firstname, "Last name: ", lastname, "\nAmount Paid: ",amount_paid)
+                print("ID: ", student_id, "\nAddress: ", student_address, "\nPhone Number: ", phone_number)
+                print("Term Period: ", term_period, "\nClass Type: ", class_type, "\nLevel: ", class_number, "\nBalance: ", balance)
+                # print("Registration status", registration_status)
+                print("------------------------------------------")
+                # make sure to update the path syntax for other OS
+                filepath = cwd+"\\data.xlsx"
+                
+                if not os.path.exists(filepath):
+                    workbook = openpyxl.Workbook()
+                    sheet = workbook.active
+                    heading = ["ID", "First Name", "Last Name", "Phone Number", "Address","Term Period", "Class Type", "Level", "Amount Paid", "Balance Due"]
+                    sheet.append(heading)
+                    workbook.save(filepath)
+                workbook = openpyxl.load_workbook(filepath)
                 sheet = workbook.active
-                heading = ["ID", "First Name", "Last Name", "Phone Number", "Address","Term Period", "Class Type", "Level"]
-                sheet.append(heading)
+                sheet.append([student_id, firstname, lastname, phone_number, student_address, term_period, class_type, class_number, amount_paid, balance])
                 workbook.save(filepath)
-            workbook = openpyxl.load_workbook(filepath)
-            sheet = workbook.active
-            sheet.append([student_id, firstname, lastname, phone_number, student_address, term_period, class_type, class_number])
-            workbook.save(filepath)
+
+            elif int(amount_paid) > total_cost:
+                tkinter.messagebox.showwarning(title="Error", message="Amount cannot be more than 5000.")
+                # os._exit(1)
                 
         else:
             tkinter.messagebox.showwarning(title="Error", message="First name and last name are required.")
@@ -60,6 +69,7 @@ def clear_form():
     formField_list[5].delete(0, tkinter.END)
     formField_list[6].delete(0, tkinter.END)
     formField_list[7].delete(0, tkinter.END)
+    formField_list[8].delete(0, tkinter.END)
 
 
 def set_form_label_frame(parentFrame, frameTxt=None, r=0, c=0):
@@ -117,6 +127,10 @@ def setup_form(frame):
     phone_number_entry = set_form_entry(student_info_frame, 4,1)
     formField_list.append(phone_number_entry)
 
+    set_form_label(student_info_frame,"Amount Paid", 5, 0)
+    amount_paid_entry = set_form_entry(student_info_frame, 5,1)
+    formField_list.append(amount_paid_entry)
+
 
     for widget in student_info_frame.winfo_children():
         widget.grid_configure(padx=10, pady=5)
@@ -136,7 +150,6 @@ def setup_form(frame):
     class_number_spinbox = set_form_spinbox(register_status_frame,1,6, 1, 3)
     formField_list.append(class_number_spinbox)
 
-    print("register status \n")
     for widget in register_status_frame.winfo_children():
         widget.grid_configure(padx=10, pady=5)
 
